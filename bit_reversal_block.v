60c91d4cb23ee95d87413e6aca3aaac18596c178
Mohanraj <117710598+mj1069@users.noreply.github.com>
2024-01-24 08:57:21 +0530

    Create bit_reversal_block.v

---
+++ bit_reversal_block.v
@@ -0,0 +1,16 @@
+module bit_reversal_block(
+  input x,
+  input y,
+  input s,
+  output m,
+  output n
+);
+
+wire a1, a2, b1, b2;
+
+demux2x1 m1(.x(x), .s(s), .m(a1), .n(b1));
+demux2x1 m2(.x(y), .s(s), .m(a2), .n(b2));
+mux2x1 m3(.a(~a1), .b(~b1), .s(s), .c(m));
+mux2x1 m4(.a(~a2), .b(~b22 ,.s(s) ,.c(n));
+
+endmodule
