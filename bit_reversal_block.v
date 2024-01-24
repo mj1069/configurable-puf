83b677c95f1b31d4c3e56192ec82f7ca3aae7886
Mohanraj <117710598+mj1069@users.noreply.github.com>
2024-01-24 20:23:46 +0530

    Update bit_reversal_block.v

--- bit_reversal_block.v
+++ bit_reversal_block.v
@@ -8,8 +8,8 @@ module bit_reversal_block(
 
 wire a1, a2, b1, b2;
 
-demux2x1 m1(.x(x), .s(s), .m(a1), .n(b1));
-demux2x1 m2(.x(y), .s(s), .m(a2), .n(b2));
+demux1x2 m1(.x(x), .s(s), .m(a1), .n(b1));
+demux1x2 m2(.x(y), .s(s), .m(a2), .n(b2));
 mux2x1 m3(.a(~a1), .b(~b1), .s(s), .c(m));
 mux2x1 m4(.a(~a2), .b(~b22 ,.s(s) ,.c(n));
 
